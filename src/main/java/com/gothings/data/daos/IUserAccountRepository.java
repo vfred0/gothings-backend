d66a4883abda86a268504b9583a9d8575d097858
Víctor Arreaga <arreagav17@gmail.com>
2024-02-14 22:02:53 -0500

    Implements user account resources

--- src/main/java/com/gothings/data/daos/IUserAccountRepository.java
+++ src/main/java/com/gothings/data/daos/IUserAccountRepository.java
@@ -2,10 +2,16 @@ package com.gothings.data.daos;
 
 
 import com.gothings.data.entities.identity.UserAccount;
+import com.gothings.data.entities.identity.UserRole;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 
 public interface IUserAccountRepository extends IRepository<UserAccount, UUID> {
     Optional<UserAccount> findByUsername(String username);
+
+    @Query("SELECT ua.roles FROM UserAccount ua, UserRole ur WHERE ua.username = :username")
+    Set<UserRole> findRolesByUsername(String username);
 }
